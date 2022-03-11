a1dd2c995463a3efd016c3acb2ef590094d69c58
alusch7 <luschera@mcmaster.ca>
2022-03-11 00:56:34 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alden Luscher on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
